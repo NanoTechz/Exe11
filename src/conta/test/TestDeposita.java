0809965292bddd639dbfea8d12b33212c6c37c6c
Nanson Augusto <nanson.augusto@hotmail.com>
2013-11-08 12:05:32 -0200

    Tratando a exception com try catch.

--- src/conta/test/TestDeposita.java
+++ src/conta/test/TestDeposita.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import conta.Conta;
+import conta.ValorInvalidoException;
 
 public class TestDeposita {
 
@@ -14,6 +15,17 @@ public class TestDeposita {
 		
 		conta.deposita(-100);
 		
+	}
+	
+	@Test 
+	public void testPrint(){
+		try{
+			Conta conta = new Conta(null, 171);
+			conta.deposita(-100);
+		} catch(ValorInvalidoException e){
+			System.out.println(e.getMessage());
+		}
+
 	}
 
 }
